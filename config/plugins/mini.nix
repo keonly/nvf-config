33cbed74ce276c280b429dc16fcf32e02f986980
keonly <70371393+keonly@users.noreply.github.com>
2025-06-04 11:51:30 +0900

    feat(plugins/snacks): replace mini.starter with snacks.dashboard

--- config/plugins/mini.nix
+++ config/plugins/mini.nix
@@ -28,57 +28,7 @@
     pairs.enable = true;
     snippets.enable = true;
     splitjoin.enable = true;
-    # statusline.enable = true;
     surround.enable = true;
-    trailspace.enable = true;
     visits.enable = true;
-  };
-
-  vim.lazy.plugins = {
-    "mini-starter" = {
-      package = "mini-starter";
-      setupModule = "mini.starter";
-      after =
-        # lua
-        ''
-          local ascii_art = [[
-                ___           ___                       ___
-               /\__\         /\__\          ___        /\__\
-              /::|  |       /:/  /         /\  \      /::|  |
-             /:|:|  |      /:/  /          \:\  \    /:|:|  |
-            /:/|:|  |__   /:/__/  ___      /::\__\  /:/|:|__|__
-           /:/ |:| /\__\  |:|  | /\__\  __/:/\/__/ /:/ |::::\__\
-           \/__|:|/:/  /  |:|  |/:/  / /\/:/  /    \/__/~~/:/  /
-               |:/:/  /   |:|__/:/  /  \::/__/           /:/  /
-               |::/  /     \::::/__/    \:\__\          /:/  /
-               /:/  /       ~~~~         \/__/         /:/  /
-               \/__/                                   \/__/
-
-          ]]
-
-          local greetings = function()
-            local hour = tonumber(vim.fn.strftime("%H"))
-            -- [04:00, 12:00) - morning, [12:00, 20:00) - day, [20:00, 04:00) - evening
-            local part_id = math.floor((hour + 4) / 8) + 1
-            local day_part = ({ "evening", "morning", "afternoon", "evening" })[part_id]
-            local username = vim.loop.os_get_passwd()["username"] or "USERNAME"
-
-            return ("Good %s, %s"):format(day_part, username)
-          end
-
-          local curr_time = function()
-            local version = vim.version()
-            local print_version = "NVIM v" .. version.major .. "." .. version.minor .. "." .. version.patch
-            local datetime = os.date("%Y/%m/%d")
-
-            return print_version .. " - " .. datetime
-          end
-
-          require("mini.starter").setup({
-            header = ascii_art .. greetings(),
-            footer = curr_time(),
-          })
-        '';
-    };
   };
 }
--- config/plugins/utility/snacks-nvim.nix
+++ config/plugins/utility/snacks-nvim.nix
@@ -1,5 +1,107 @@
-{
+{lib, ...}: {
   vim.utility.snacks-nvim = {
     enable = true;
+
+    setupOpts = {
+      dashboard = {
+        preset.header = builtins.concatStringsSep "\n" [
+          "        ___           ___                       ___         "
+          "       /\\__\\         /\\__\\          ___        /\\__\\        "
+          "      /::|  |       /:/  /         /\\  \\      /::|  |       "
+          "     /:|:|  |      /:/  /          \\:\\  \\    /:|:|  |       "
+          "    /:/|:|  |__   /:/__/  ___      /::\\__\\  /:/|:|__|__     "
+          "   /:/ |:| /\\__\\  |:|  | /\\__\\  __/:/\\/__/ /:/ |::::\\__\\    "
+          "   \\/__|:|/:/  /  |:|  |/:/  / /\\/:/  /    \\/__/~~/:/  /    "
+          "       |:/:/  /   |:|__/:/  /  \\::/__/           /:/  /     "
+          "       |::/  /     \\::::/__/    \\:\\__\\          /:/  /      "
+          "       /:/  /       ~~~~         \\/__/         /:/  /       "
+          "       \\/__/                                   \\/__/        "
+        ];
+        sections = [
+          {section = "header";}
+          {
+            section = "terminal";
+            cmd =
+              # bash
+              ''
+                #!/bin/sh
+
+                hour=$(date +"%H")
+                hour=$((10#$hour))
+
+                part_id=$(( (hour + 4) / 8 + 1 ))
+
+                case $part_id in
+                  1) day_part="evening" ;;
+                  2) day_part="morning" ;;
+                  3) day_part="afternoon" ;;
+                  4) day_part="evening" ;;
+                esac
+
+                username=$(printf "%.4s" "$(whoami 2>/dev/null || echo USERNAME)")
+                greeting=$(printf "Good %s, %s" "$day_part" "$username")
+
+                case $day_part in
+                  morning|evening) greeting_len=18 ;;
+                  afternoon)       greeting_len=20 ;;
+                esac
+
+                total_width=60
+                pad=$(( (total_width - greeting_len) / 2 ))
+
+                printf "%*s%s%*s\n" "$pad" "" "$greeting" "$((total_width - pad - greeting_len))" ""
+              '';
+            hl = "header";
+            height = 2;
+            padding = 1;
+          }
+          {
+            icon = " ";
+            title = "Keymaps";
+            section = "keys";
+            indent = 2;
+            padding = 1;
+          }
+          {
+            icon = " ";
+            title = "Recent Files";
+            section = "recent_files";
+            indent = 2;
+            padding = 1;
+          }
+          {
+            icon = " ";
+            title = "Projects";
+            section = "projects";
+            indent = 2;
+            padding = 1;
+          }
+          {
+            section = "terminal";
+            cmd =
+              # bash
+              ''
+                #!/bin/sh
+
+                nvim_version=$(nvim --version | head -n 1 | awk '{print $2}')
+                print_version=" $nvim_version"
+
+                datetime=$(date +"%Y/%m/%d")
+                print_datetime=" $datetime"
+
+                greeting="$print_version - $print_datetime" # 9 + 3 + 12 = 24 chars
+                greeting_len=24
+                total_width=60
+                pad=$(( (total_width - greeting_len) / 2 ))
+
+                printf "\n%*s%s%*s" "$pad" "" "$greeting" "$((total_width - pad - greeting_len))" ""
+              '';
+            hl = "NonText";
+            height = 2;
+            padding = 1;
+          }
+        ];
+      };
+    };
   };
 }
