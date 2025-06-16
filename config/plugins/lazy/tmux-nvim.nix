2680d2200f82a89735ab4e79fd3a23c51e8abeb8
keonly <70371393+keonly@users.noreply.github.com>
2025-06-16 11:43:36 +0900

    feat(plugins/tmux-nvim): use tmux instead of zellij

---
+++ config/plugins/lazy/tmux-nvim.nix
@@ -0,0 +1,19 @@
+{pkgs, ...}: {
+  vim.lazy.plugins."tmux.nvim" = {
+    package = pkgs.vimPlugins.tmux-nvim;
+    setupModule = "tmux";
+
+    setupOpts = {
+      swap.enable_default_keybindings = false;
+    };
+
+    after =
+      # lua
+      ''
+        vim.keymap.set("n", "<C-h>", require("tmux").move_left, {desc = "Tmux move left"})
+        vim.keymap.set("n", "<C-j>", require("tmux").move_bottom, {desc = "Tmux move down"})
+        vim.keymap.set("n", "<C-k>", require("tmux").move_top, {desc = "Tmux move up"})
+        vim.keymap.set("n", "<C-l>", require("tmux").move_right, {desc = "Tmux move right"})
+      '';
+  };
+}
