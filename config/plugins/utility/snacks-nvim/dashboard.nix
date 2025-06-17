eb58589ee20fb13596a248968177be0f13ecca74
keonly <70371393+keonly@users.noreply.github.com>
2025-06-17 12:41:34 +0900

    feat(plugins/snacks.dashboard): show a random eevee

--- config/plugins/utility/snacks-nvim/dashboard.nix
+++ config/plugins/utility/snacks-nvim/dashboard.nix
@@ -1,6 +1,14 @@
-{lib, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  vim.extraPackages = [
+    pkgs.krabby
+  ];
+
   vim.utility.snacks-nvim.setupOpts.dashboard = let
-    width = 30;
+    width = 40;
   in {
     inherit width;
     sections =
@@ -8,15 +16,32 @@
       # lua
       ''
         {
-          {
-            align = "center",
-            text = {
-              {'   /| ､       \n'},
-              {'  (°､ ｡ 7     \n'},
-              {'   |､  ~ヽ    \n'},
-              {'   じしf_,)〳 \n'},
-            },
-          },
+          function()
+            local eevees = {
+              vaporeon = 16,
+              jolteon = 17,
+              flareon = 18,
+              espeon = 16,
+              umbreon = 15,
+              leafeon = 17,
+              glaceon = 16,
+              sylveon = 17,
+            }
+
+            local keys = {}
+            for k in pairs(eevees) do
+              keys[#keys+1] = k
+            end
+            local random_eevee = keys[math.random(#keys)]
+
+            return {
+              section = "terminal",
+              cmd = "krabby name ".. random_eevee .. " --no-title",
+              height = eevees[random_eevee],
+              indent = (random_eevee == "vaporeon") and 0 or 4,
+              padding = 1,
+            }
+          end,
           function()
             local hour = tonumber(vim.fn.strftime('%H'))
             local part_id = math.floor((hour + 4) / 8) + 1
