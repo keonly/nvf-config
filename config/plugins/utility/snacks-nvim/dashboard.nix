e5f6da85e6914a887855f405429081faa9dd2e6a
keonly <70371393+keonly@users.noreply.github.com>
2025-06-18 16:15:53 +0900

    Revert "feat(plugins/snacks.dashboard): show a random eevee"
    This reverts commit eb58589ee20fb13596a248968177be0f13ecca74.

--- config/plugins/utility/snacks-nvim/dashboard.nix
+++ config/plugins/utility/snacks-nvim/dashboard.nix
@@ -1,14 +1,6 @@
-{
-  pkgs,
-  lib,
-  ...
-}: {
-  vim.extraPackages = [
-    pkgs.krabby
-  ];
-
+{lib, ...}: {
   vim.utility.snacks-nvim.setupOpts.dashboard = let
-    width = 40;
+    width = 30;
   in {
     inherit width;
     sections =
@@ -16,32 +8,15 @@
       # lua
       ''
         {
-          function()
-            local eevees = {
-              vaporeon = 16,
-              jolteon = 17,
-              flareon = 18,
-              espeon = 16,
-              umbreon = 15,
-              leafeon = 17,
-              glaceon = 16,
-              sylveon = 17,
-            }
-
-            local keys = {}
-            for k in pairs(eevees) do
-              keys[#keys+1] = k
-            end
-            local random_eevee = keys[math.random(#keys)]
-
-            return {
-              section = "terminal",
-              cmd = "krabby name ".. random_eevee .. " --no-title",
-              height = eevees[random_eevee],
-              indent = (random_eevee == "vaporeon") and 0 or 4,
-              padding = 1,
-            }
-          end,
+          {
+            align = "center",
+            text = {
+              {'   /| ､       \n'},
+              {'  (°､ ｡ 7     \n'},
+              {'   |､  ~ヽ    \n'},
+              {'   じしf_,)〳 \n'},
+            },
+          },
           function()
             local hour = tonumber(vim.fn.strftime('%H'))
             local part_id = math.floor((hour + 4) / 8) + 1
