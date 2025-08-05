a35dd3b9bdd99d95b458568ecc6b5512c894fd25
keonly <70371393+keonly@users.noreply.github.com>
2025-08-05 12:29:46 +0900

    fix: only enable wl-copy on linux

--- config/neovim/clipboard.nix
+++ config/neovim/clipboard.nix
@@ -1,9 +1,12 @@
-{
+{pkgs, ...}: {
   vim.clipboard = {
     enable = true;
 
     providers = {
-      wl-copy.enable = true;
+      wl-copy.enable =
+        if pkgs.stdenv.isDarwin
+        then false
+        else true;
     };
     registers = "unnamedplus";
   };
